4239586fc54515e4aca7bb9b53b501746f8368e7
Michael Barton <michael@mbarton.cc>
2024-04-02 21:19:53 +0100

    Add API keys page to user profile
    Copied structure from https://github.com/spirobel/monero-discourse-subscriptions/commit/871d053868a52b2e4e6d0de3216853b6f068bd82

--- plugin.rb
+++ plugin.rb
@@ -3,3 +3,16 @@
 # version: 0.0.1
 # authors: Michael Barton, RCPCH
 # url: https://github.com/rcpch/discourse-apim
+
+register_svg_icon "server"
+
+after_initialize do
+  Discourse::Application.routes.append do
+    %w[users u].each do |root_path|
+      get "#{root_path}/:username/api-keys" => "users#show",
+        :constraints => {
+          username: RouteFormat.username,
+        }
+    end
+  end
+end
