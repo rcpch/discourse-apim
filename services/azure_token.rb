97edb442675139da785d44234789c56771aa4327
Michael Barton <michael@mbarton.cc>
2024-05-14 10:56:51 +0100

    Generate azure token

--- services/azure_token.rb
+++ services/azure_token.rb
@@ -1,2 +1,16 @@
+require "base64"
+
 class AzureToken
+  def self.generate
+    identifier = "integration"
+    key = "TODO CONFIG"
+    
+    expiry = (Time.now + (60 * 60)).strftime("%Y-%m-%dT%H:%M:%S.0000000Z")
+    string_to_sign = "#{identifier}\n#{expiry}"
+    
+    digest = OpenSSL::HMAC.digest("SHA512", key, string_to_sign)
+    sn = Base64.strict_encode64(digest)
+    
+    return "uid=integration&ex=#{expiry}&sn=#{sn}"
+  end
 end
