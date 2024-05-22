c6ad771a9e0c15aa7add9083514f571c1b2b5310
Michael Barton <michael@mbarton.cc>
2024-05-22 13:57:30 +0100

    Return all the fields I think might be useful

--- services/usage_reporting.rb
+++ services/usage_reporting.rb
@@ -2,22 +2,31 @@ def generate_report(usage_data_list)
   ret = {}
 
   usage_data_list.each { |usage_data|
-    subscriptionId = usage_data['subscriptionId']
-    
-    # https://learn.microsoft.com/en-us/rest/api/apimanagement/reports/list-by-subscription?view=rest-apimanagement-2022-08-01&tabs=HTTP#reportrecordcontract
-    callCountTotal = usage_data['callCountTotal']
+    subscription_id = usage_data["subscriptionId"]
 
-    if callCountTotal > 0
-      ret[subscriptionId] = {
-        # 2XX, 3XX
-        "callCountSuccess": usage_data['callCountSuccess'],
-        # 4XX
-        "callCountBlocked": usage_data['callCountBlocked'],
-        # 5XX
-        "callCountFailed": usage_data['callCountFailed'],
-        # ??? (the docs say "Number of other calls" lol)
-        "callCountOther": usage_data['callCountOther']
-      }
+    # https://learn.microsoft.com/en-us/rest/api/apimanagement/reports/list-by-subscription?view=rest-apimanagement-2022-08-01&tabs=HTTP#reportrecordcontract
+    fields_to_save = [
+      "callCountTotal",
+      # 2XX, 3XX
+      "callCountSuccess",
+      # 4XX
+      ":callCountBlocked",
+      # 5XX
+      ":callCountFailed",
+      # ??? (the docs say "Number of other calls" lol)
+      "callCountOther",
+      # Elapsed time (I think)
+      "apiTimeMin",
+      "apiTimeMax",
+      "apiTimeAvg",
+      # Backend time
+      "serviceTimeMin",
+      "serviceTimeMax",
+      "serviceTimeAvg"
+    ]
+
+    if usage_data['callCountTotal'] > 0
+      ret[subscription_id] = usage_data.slice(*fields_to_save)
     end
   }
   
